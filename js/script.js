7bccd5f738b7a69e089c9387b7a9daf6ce710c19
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2022-11-07 10:58:45 +0700

    started

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const str = "Hello world";
+console.log(str);
+
+const number1 = 10;
+const number2 = 14;
+console.log(`number1 = ${number1} and number2 = ${number2}`);
+console.log(`number1 + number2 = ${number1 + number2}`);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(``)
