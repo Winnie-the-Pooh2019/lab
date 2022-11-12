e0f1a50e14e03b33cdbb5bebec5dd4c478b78a38
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2022-11-12 15:48:03 +0700

    lab7 done

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ console.log(`number1 + number2 = ${number1 + number2}`);
 
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
-console.log(``)
+console.log(`n1 + n2 = ${number1 + number2}
+n1 - n2 = ${number1 - number2}
+n1 * n2 = ${number1 * number2}
+n1 / n2 = ${number1 / number2}
+n1^2 = ${Math.pow(number1, 2)}
+number % number2 = ${number1 % number2}`);
+
+// replace all "o" characters with "O"
+console.log(str.replace(/o/g, "O"));
+
+// split the string into separate substrings
+console.log(str.split(' '));
